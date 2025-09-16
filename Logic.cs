8c70ab3bdc377b9b45f7baf158419ca98d10d783
Maluska10 <salamona@csp.edu>
2025-09-15 20:38:30 -0500

    Adjusted SaveQuizQuestions and ReadQUizQuestions methods in Logic.cs

--- Logic.cs
+++ Logic.cs
@@ -9,16 +9,20 @@ namespace QuizMaker
 {
     class Logic
     {
+        public List<QuizForm> fileQuestions;
         static readonly Random random = new Random();
 
         public static string path = @"C:\Users\alexs\source\repos\QuizMaker\QuizList.xml";
         private static XmlSerializer writer = new XmlSerializer(typeof(List<QuizForm>));
         public static void SaveQuizQuestions(List<QuizForm> QuizList)
-        {
-            using (FileStream file = File.Create(path))
+        {                
+                List<QuizForm> fileQuestions = ReadQuizQuestions();
+                fileQuestions.AddRange(QuizList);
+            //Read the list from file
+            using (FileStream file = File.OpenRead(path))
             {
-                writer.Serialize(file, QuizList);
-            }
+                writer.Serialize(file, fileQuestions);
+            }     
         }
         public static List<QuizForm> ReadQuizQuestions()
         {
